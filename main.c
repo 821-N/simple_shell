54ad0e8454fdf2c6e0112ff51619b08eab2d941c
chriswill88 <williechri79@gmail.com>
2019-08-17 20:42:46 -0400

    save for merge

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include "shell.h"
 
+
 int main(int argc, char **argv, char **env)
 {
 	char *input, **args, *file_path, **sargs;
@@ -23,7 +24,19 @@ int main(int argc, char **argv, char **env)
 		print_args(args); /* debug */
 
 		/* search for command in PATH */
-		//file_path = get_file(args[0], path);
+		file_path = search_path(args[0], path);
+		if (file_path)
+			printf("Found: %s\n", file_path);
+		else
+			puts("Couldn't find");
+		parchild = fork();
+		if (parchild < 0)
+			printf("fail\n");
+		else if (parchild == 0)
+		{
+			execve(args[0], args, NULL);
+		}
+		
 		/* run */
 		//run_program(file_path, args, env);
 	}
